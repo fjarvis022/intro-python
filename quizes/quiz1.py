cf199f99bca49c5e3dd90fdc833a7a1046bb562a
fjarvis022 <144710797+fjarvis022@users.noreply.github.com>
2023-09-15 16:22:41 +0000

    done quiz

--- quizes/quiz1.py
+++ quizes/quiz1.py
@@ -1,11 +1,15 @@
 # 1. In your on words, describe what a variable is.
-
+"Its basically a name like frank or it can be a thing like Cat" 
 # 2. Name three (3) operators we've learned about in class and describe 
+"arithmetic, comparison, and assignment"
 # a situation where you would use each one. 
-
+"you use arithmetic in math"
 # 3. Describe the difference between camal case and pascal case.
-
+"in pascal every word starts with a capital and in camel the second word"
+"of a variable and every word after starts with a capital letter"
 # 4. Which operator is being used in the example below
+"assingment operator"
+
 numberOfApples = 10
 numberOfOranges = 12
 
@@ -16,6 +20,8 @@ numberOfApples < numberOfOranges
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
 
+giftcard1price = 50.00 giftcard2price = 35.50 giftcard3price = 100.00 fullgiftcardamount = 185.5
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
 
@@ -25,12 +31,18 @@ numberOfApples < numberOfOranges
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
 
+input("what is your full name")
+
+
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
+I thnik boolean would be best for this because it gives true or false questions and I think its the perfet way to figure it out
+
 
 # 9. In your own words, describe what a program is?
-
+its something built into a website or whatever it might be that helps it run or helps it run something
 # 10. Describe the difference between a float and an integer. 
+float is a decimal boolean is not 
